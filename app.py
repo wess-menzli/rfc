61a490a8b8018ccbe60cc99c4367533eb97641c9
wess-menzli <Wessim.MENZLI@rfc.com.tn>
2025-09-07 21:19:14 +0200

    Update app.py1

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-from fastapi import FastAPI, Query
-import os
-import pandas as pd
-from prediction_finale import predict_next_leave  # 👈 import your function
+from fastapi import FastAPI
+from prediction_finale import predict_next_leave
 
-# Create the FastAPI app
 app = FastAPI()
 
 @app.get("/predict")
-def predict(name: str = Query(..., description="Nom complet de l'employé")):
-    try:
-        result = predict_next_leave(name)
-        return {"employee": name, "result": result}
-    except Exception as e:
-        return {"error": str(e)}
+def predict(Nom_Employe: str):
+    return {"result": predict_next_leave(Nom_Employe)}
